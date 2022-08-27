db1a86f834eff279f16a9c3e5a50075a69795b38
Jay BCH <oasis-nft@protonmail.com>
2022-08-27 21:26:18 +0200

    Dogechain

---
+++ contracts/NFTEXV3.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//https://github.com/TheGreatHB/NFTEX/blob/main/contracts/NFTEX.sol
+
+
+contract NFTEXV3 is ERC721Holder, Ownable {
+
+  struct Order {
+    uint8 orderType;  //0:Fixed Price, 1:Dutch Auction, 2:English Auction
+    address seller;
+    IERC721 token;
+    uint256 tokenId;
+    uint256 startPrice;
+    uint256 endPrice;
+    uint256 startBlock;
+    uint256 endBlock;
+    uint256 lastBidPrice;
+    address lastBidder;
+    bool isSold;
+    bool isCancelled;
+  }
+
+  mapping (IERC721 => mapping (uint256 => bytes32[])) public orderIdByToken;
+  mapping (address => bytes32[]) public orderIdBySeller;
+  mapping (bytes32 => Order) public orderInfo;
+
+  address public feeAddress;
+  uint16 public feePercent;
+
+  event MakeOrder(IERC721 indexed token, uint256 id, bytes32 indexed hash, address indexed seller);
+  event CancelOrder(IERC721 indexed token, uint256 id, bytes32 indexed hash, address indexed seller);
+  event Bid(IERC721 indexed token, uint256 id, bytes32 indexed hash, address indexed bidder, uint256 bidPrice);
+  event Claim(IERC721 indexed token, uint256 id, bytes32 indexed hash, address seller, address indexed taker, uint256 price);
+
+  constructor(uint16 _feePercent) {
+    require(_feePercent <= 1000, "Input value is more than 10%");
+    feeAddress = msg.sender;
+    feePercent = _feePercent;
+  }
+
+
+  // view fx
+  function getCurrentPrice(bytes32 _order) public view returns (uint256) {
+    Order storage o = orderInfo[_order];
+    uint8 orderType = o.orderType;
+    if (orderType == 0) {
+      return o.startPrice;
+    } else if (orderType == 2) {
+      uint256 lastBidPrice = o.lastBidPrice;
+      return lastBidPrice == 0 ? o.startPrice : lastBidPrice;
+    } else {
+      uint256 _startPrice = o.startPrice;
+      uint256 _startBlock = o.startBlock;
+      uint256 tickPerBlock = (_startPrice - o.endPrice) / (o.endBlock - _startBlock);
+      return _startPrice - ((block.number - _startBlock) * tickPerBlock);
+    }
+  }
+
+  function tokenOrderLength(IERC721 _token, uint256 _id) public view returns (uint256) {
+    return orderIdByToken[_token][_id].length;
+  }
+
+  function sellerOrderLength(address _seller) external view returns (uint256) {
+    return orderIdBySeller[_seller].length;
+  }
+
+  function bulkList(IERC721 _token, uint256[] memory _ids, uint256 _startPrice, uint256 _endPrice, uint256 _endBlock, uint256 _type) public {
+    require(_ids.length > 0, "At least 1 ID must be supplied");
+
+    if(_type == 0) {
+      for(uint i=0; i < _ids.length; i++) {
+        _makeOrder(0, _token, _ids[i], _startPrice, 0, _endBlock);
+      }
+    }
+
+    if(_type == 1) {
+      require(_startPrice > _endPrice, "End price higher than start");
+      for(uint i=0; i < _ids.length; i++) {
+        _makeOrder(1, _token, _ids[i], _startPrice, _endPrice, _endBlock);
+      }
+    }
+
+    if(_type == 2) {
+      for(uint i=0; i < _ids.length; i++) {
+        _makeOrder(2, _token, _ids[i], _startPrice, 0, _endBlock);
+      }
+    }
+  }
+
+  // make order fx
+  //0:Fixed Price, 1:Dutch Auction, 2:English Auction
+  function dutchAuction(IERC721 _token, uint256 _id, uint256 _startPrice, uint256 _endPrice, uint256 _endBlock) public {
+    require(_startPrice > _endPrice, "End price higher than start");
+    _makeOrder(1, _token, _id, _startPrice, _endPrice, _endBlock);
+  }  //sp != ep
+
+  function englishAuction(IERC721 _token, uint256 _id, uint256 _startPrice, uint256 _endBlock) public {
+    _makeOrder(2, _token, _id, _startPrice, 0, _endBlock);
+  } //ep=0. for gas saving.
+
+  function fixedPrice(IERC721 _token, uint256 _id, uint256 _price, uint256 _endBlock) public {
+    _makeOrder(0, _token, _id, _price, 0, _endBlock);
+  }  //ep=0. for gas saving.
+
+  function _makeOrder(
+    uint8 _orderType,
+    IERC721 _token,
+    uint256 _id,
+    uint256 _startPrice,
+    uint256 _endPrice,
+    uint256 _endBlock
+  ) internal {
+    require(_endBlock > block.number, "Duration must be more than zero");
+
+    //push
+    bytes32 hash = _hash(_token, _id, msg.sender);
+    orderInfo[hash] = Order(_orderType, msg.sender, _token, _id, _startPrice, _endPrice, block.number, _endBlock, 0, address(0), false, false);
+    orderIdByToken[_token][_id].push(hash);
+    orderIdBySeller[msg.sender].push(hash);
+
+    //check if seller has a right to transfer the NFT token. safeTransferFrom.
+    _token.safeTransferFrom(msg.sender, address(this), _id);
+
+    emit MakeOrder(_token, _id, hash, msg.sender);
+  }
+
+  function _hash(IERC721 _token, uint256 _id, address _seller) internal view returns (bytes32) {
+    return keccak256(abi.encodePacked(block.number, _token, _id, _seller));
+  }
+
+  // take order fx
+  // you have to pay only BCH for bidding and buying.
+
+  // In this contract, since send function is used instead of transfer or low-level call function,
+  // if a participant is a contract, it must have receive payable function.
+  // But if it has some code in either receive or fallback fx, they might not be able to receive their BCH.
+  // Even though some contracts can't receive their BCH, the transaction won't be failed.
+
+  // Bids must be at least 5% higher than the previous bid.
+  // If someone bids in the last 5 minutes of an auction, the auction will automatically extend by 5 minutes.
+  function bid(bytes32 _order) payable external {
+    Order storage o = orderInfo[_order];
+    uint256 endBlock = o.endBlock;
+    uint256 lastBidPrice = o.lastBidPrice;
+    address lastBidder = o.lastBidder;
+
+    require(o.orderType == 2, "only for English Auction");
+    require(o.isCancelled == false, "Canceled order");
+    require(block.number <= endBlock, "Auction has ended");
+    require(o.seller != msg.sender, "Can not bid on your own order");
+
+    if (lastBidPrice != 0) {
+      require(msg.value >= lastBidPrice + (lastBidPrice / 20), "low price bid");  //5%
+    } else {
+      require(msg.value >= o.startPrice && msg.value > 0, "low price bid");
+    }
+
+    if (block.number > endBlock - 150) {  // 150 blocks = 5 mins on dogechain.
+      o.endBlock = endBlock + 150;
+    }
+
+    o.lastBidder = msg.sender;
+    o.lastBidPrice = msg.value;
+
+    if (lastBidPrice != 0) {
+      payable(lastBidder).transfer(lastBidPrice);
+    }
+
+    emit Bid(o.token, o.tokenId, _order, msg.sender, msg.value);
+  }
+
+  function buyItNow(bytes32 _order) payable external {
+    Order storage o = orderInfo[_order];
+    uint256 endBlock = o.endBlock;
+    require(endBlock != 0, "Canceled order");
+    require(o.isCancelled == false, "Canceled order");
+    require(endBlock > block.number, "Auction has ended");
+    require(o.orderType < 2, "It's a English Auction");
+    require(o.isSold == false, "Already sold");
+
+    uint256 currentPrice = getCurrentPrice(_order);
+    require(msg.value >= currentPrice, "Price error");
+
+    o.isSold = true;    //reentrancy proof
+
+    uint256 fee = currentPrice * feePercent / 10000;
+    payable(o.seller).transfer(currentPrice - fee);
+    payable(feeAddress).transfer(fee);
+    if (msg.value > currentPrice) {
+      payable(msg.sender).transfer(msg.value - currentPrice);
+    }
+
+    o.token.safeTransferFrom(address(this), msg.sender, o.tokenId);
+
+    emit Claim(o.token, o.tokenId, _order, o.seller, msg.sender, currentPrice);
+  }
+
+  // both seller and taker can call this fx in English Auction. Probably the taker(last bidder) might call this fx.
+  // In both DA and FP, buyItNow fx include claim fx.
+  function claim(bytes32 _order) public {
+    Order storage o = orderInfo[_order];
+    address seller = o.seller;
+    address lastBidder = o.lastBidder;
+    require(o.isSold == false, "Already sold");
+    require(o.isCancelled == false, "Already cancelled");
+    require(seller == msg.sender || lastBidder == msg.sender, "Access denied");
+    require(o.orderType == 2, "English Auction only");
+    require(block.number > o.endBlock, "Auction has not ended");
+
+    IERC721 token = o.token;
+    uint256 tokenId = o.tokenId;
+    uint256 lastBidPrice = o.lastBidPrice;
+
+    uint256 fee = lastBidPrice * feePercent / 10000;
+
+    o.isSold = true;
+
+    payable(seller).transfer(lastBidPrice - fee);
+    payable(feeAddress).transfer(fee);
+    token.safeTransferFrom(address(this), lastBidder, tokenId);
+
+    emit Claim(token, tokenId, _order, seller, lastBidder, lastBidPrice);
+  }
+
+  function bulkClaim(bytes32[] memory _ids) public {
+    require(_ids.length > 0, "At least 1 ID must be supplied");
+    for(uint i=0; i < _ids.length; i++) {
+      claim(_ids[i]);
+    }
+  }
+
+  function cancelOrder(bytes32 _order) public {
+    Order storage o = orderInfo[_order];
+    require(o.seller == msg.sender, "Access denied");
+    require(o.lastBidPrice == 0, "Bidding exist"); //for EA. but even in DA, FP, seller can withdraw his/her token with this fx.
+    require(o.isSold == false, "Already sold");
+    require(o.isCancelled == false, "Already cancelled");
+
+    IERC721 token = o.token;
+    uint256 tokenId = o.tokenId;
+
+    o.endBlock = 0;   //0 endBlock means the order was canceled. - retained for backwards compatibility
+    o.isCancelled = true;
+
+    token.safeTransferFrom(address(this), msg.sender, tokenId);
+    emit CancelOrder(token, tokenId, _order, msg.sender);
+  }
+
+  function bulkCancel(bytes32[] memory _ids) public {
+    require(_ids.length > 0, "At least 1 ID must be supplied");
+    for(uint i=0; i < _ids.length; i++) {
+      cancelOrder(_ids[i]);
+    }
+  }
+
+  //feeAddress must be either an EOA or a contract must have payable receive fx and doesn't have some codes in that fx.
+  //If not, it might be that it won't be receive any fee.
+  function setFeeAddress(address _feeAddress) external onlyOwner {
+    feeAddress = _feeAddress;
+  }
+
+  function updateFeePercent(uint16 _percent) external onlyOwner {
+    require(_percent <= 1000, "Input value is more than 10%");
+    feePercent = _percent;
+  }
+
+}
--- readme.md
+++ readme.md
@@ -1,33 +1,23 @@
-# Oasis Contract Information
+# Dogechain - Oasis Contract Information
+
+Contracts used by https://oasis-nft.dog (verified)
+## NFTEX exchange V3 contract (NFTEXV3.sol)
+https://explorer.dogechain.dog/address/0x3E79C89f479824Bc24b9eAD73EB8c55F322FE963/
+
+Changes in V3:
+- Cap max fee amount from 100% to 10% (currently set at 2.5%)
+- Adjusted block extension time to dogechain (150 block @ 2 seconds = 5 min)
+
+
+
+# SmartBCH - Oasis Contract Information
 
 Contracts used by https://oasis.cash/
 
 ## NFTEX exchange contract (NFTEX.sol)
-```Address:      0x657061bf5D268F70eA3eB1BCBeb078234e5Df19d```
 ### https://www.smartscan.cash/address/0x657061bf5D268F70eA3eB1BCBeb078234e5Df19d
 
-
-```Transaction: 0xfeb079da51da8e52d7243506b4d89da36f7987881ac5be7a8f2530499dd616df```
-### https://www.smartscan.cash/transaction/0xfeb079da51da8e52d7243506b4d89da36f7987881ac5be7a8f2530499dd616df
-
 ## NFTEX exchange V2 contract (NFTEXV2.sol)
-```Address:      0x3b968177551a2aD9fc3eA06F2F41d88b22a081F7```
+
 ### https://www.smartscan.cash/address/0x3b968177551a2aD9fc3eA06F2F41d88b22a081F7
 
-
-```Transaction: 0x2ce52b0e76071d404e0d71ded586bb301939b6f59b714ca4ae1499dd4667f229```
-### https://www.smartscan.cash/transaction/0x2ce52b0e76071d404e0d71ded586bb301939b6f59b714ca4ae1499dd4667f229
-
-## CashCats NFT (OasisNFT.sol)
-```Address: 0xE765026Cad648785b080E78700cBF6fa1C050d7C```
-### https://www.smartscan.cash/address/0xE765026Cad648785b080E78700cBF6fa1C050d7C
-
-```Transaction: 0x43fd9f049822f9e3f8ea7f17d41e2b2d5bf0fd9746dbfc9a9646ee92bc8f7203```
-### https://www.smartscan.cash/transaction/0x43fd9f049822f9e3f8ea7f17d41e2b2d5bf0fd9746dbfc9a9646ee92bc8f7203
-
-## Cats4Cats TokenSale Contract (OasisNFTforERC20.sol)
-```Address: 0x4c58BDCF6CE921625a71963C5Ce53BBC3bEf5c77```
-### https://www.smartscan.cash/address/0x4c58BDCF6CE921625a71963C5Ce53BBC3bEf5c77
-
-```Transaction: 0x25ec7ec994f86d4f64ac4e1a90c991834679d34c523f9acfedfbcad8ab68bbff```
-### https://www.smartscan.cash/transaction/0x25ec7ec994f86d4f64ac4e1a90c991834679d34c523f9acfedfbcad8ab68bbff
